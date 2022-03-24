c3befc30ccd89b736eec8b6c654456bbbeff35c7
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-03-24 13:19:37 +0100

    Add read_and_encode

--- read_file.py
+++ read_file.py
@@ -6,4 +6,12 @@ def read_and_encode():
     out_file = open("encoded.txt", 'wb')
     out_file.write(encoded_file)
 
+def read_and_decode():
+    in_file = open("encoded.txt", 'rb').read()
+    encoded_file = c.encode_byte_array(in_file)
+    encoded_file = c.correct_byte_array(encoded_file)
+    out_file = open("fixed.txt", 'wb')
+    out_file.write(encoded_file)
+
+
 read_and_encode()
